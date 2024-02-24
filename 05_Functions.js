0cd19968d280e0a695f7a2419a89a2b117f6415d
shubham-sutar <shubhamsutar93@gmail.com>
2024-02-24 13:16:36 +0530

    update

---
+++ 05_Functions.js
@@ -0,0 +1,14 @@
+
+
+function add(num1,num2){
+    let result = num1 + num2;
+    console.log(`result of addition ${result}`)
+}
+
+add(50,2);
+
+function sub(num1,num2){
+    return num1 - num2;
+}
+
+console.log(sub(25,5));
