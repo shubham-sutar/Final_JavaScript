95ec12fae8a851edc6d0ebaa3c2ca6bcd104827b
shubham-sutar <shubhamsutar93@gmail.com>
2024-02-24 14:20:40 +0530

    updated

--- 05_Functions.js
+++ 05_Functions.js
@@ -12,3 +12,24 @@ function sub(num1,num2){
 }
 
 console.log(sub(25,5));
+
+function greet(myName){
+    console.log(`Hello ${myName} , Welcome to the Dr. D. Y. Patil college of Engineering`);
+}
+
+greet("shubham")
+greet("Tushar")
+
+//anonymouse function
+
+let state = function(){
+    console.log("hello world");
+}
+state();
+
+
+// IIFE Immediately invoke function
+
+var result = (state = function(){
+    console.log("This is javascript");
+})();
