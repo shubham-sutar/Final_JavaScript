c20a5fabbba87dee296941ad4bf338e80bd10dbe
shubham-sutar <shubhamsutar93@gmail.com>
2024-02-24 09:07:40 +0530

    update

---
+++ 02_IfElse.js
@@ -0,0 +1,8 @@
+let Age = 19;
+if(Age > 18 ){
+    console.log("Your eligible For vote")
+}
+else{
+    console.log("Your not eligible")
+}
+
---
+++ 03_Loops.js
@@ -0,0 +1,24 @@
+
+console.log("Print 1 to 20 Numbers - ")
+for( let i = 1; i<= 20; i++ ){
+    console.log(i);
+}
+
+let i = 20;
+while( i >= 1){
+    console.log(i)
+    i--;
+}
+
+do{
+    console.log(i);
+    i++;
+}while(i <= 5);
+
+let myArr = [12,34,56,78,23,12]
+
+myArr.forEach(myFun)
+
+function myFun(items){
+    console.log(items);
+}
