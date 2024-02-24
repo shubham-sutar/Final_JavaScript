2c83c2e27fc4cf7ac2714a1cf1af5280cc1b76e3
shubham-sutar <shubhamsutar93@gmail.com>
2024-02-24 09:42:39 +0530

    chnages

--- 02_IfElse.js
+++ 02_IfElse.js
@@ -19,3 +19,15 @@ if (number % 2 == 0){
 else{
     console.log(`${number} Number is odd`)
 }
+
+//Print num is negative, positive, and Zero
+
+num = -10;
+
+if(num == 0){
+    console.log(`Number is zero`)
+} else if(num > 0){
+    console.log(`${num} is positive`)
+}else{
+    console.log(`${num} is negative`)
+}
